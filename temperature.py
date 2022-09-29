2d4b88b84c50b40430c4f27e43644d4ef670d321
sarnoult <sophie.arnoult@posteo.net>
2022-09-29 11:58:56 +0200

    refactor

--- temperature.py
+++ temperature.py
@@ -1,18 +1,33 @@
 import pandas as pd
 from matplotlib import pyplot as plt
 
-num_measurements = 25
 
-# read data from file
-data = pd.read_csv("temperatures.csv", nrows=num_measurements)
-temperatures = data["Air temperature (degC)"]
+def read_data(num_measurements):
+    """read data from file"""
+    data = pd.read_csv("temperatures.csv", nrows=num_measurements)
+    temperatures = data["Air temperature (degC)"]
+    return temperatures
 
-# compute statistics
-mean = sum(temperatures) / num_measurements
 
-# plot results
-plt.plot(temperatures, "r-")
-plt.axhline(y=mean, color="b", linestyle="--")
-plt.show()
-plt.savefig("25.png")
-plt.clf()
+def compute_mean(temperatures, num_measurements):
+    """compute statistics"""
+    return sum(temperatures) / num_measurements
+    
+
+def plot_data(temperatures, mean, fname):
+    """plot results"""
+    plt.plot(temperatures, "r-")
+    plt.title("temperatures in Helsinki")
+    plt.xlabel("measurements")
+    plt.ylabel("air temperature (deg C)")
+    plt.axhline(y=mean, color="b", linestyle="--")
+    plt.show()
+    plt.savefig(fname)
+    plt.clf()
+
+
+for measurement in [25, 100, 500]:
+    temperatures = read_data(measurement)
+    mean = compute_mean(temperatures, measurement)
+    plot_data(temperatures, mean, f"{measurement}.png")
+
